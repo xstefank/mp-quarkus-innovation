72384491cab7919f15fc98cac3048cd36bdc4b64
xstefank <xstefank122@gmail.com>
2022-10-26 23:00:45 +0200

    Add POST to test

--- src/test/java/org/acme/AvengerResourceTest.java
+++ src/test/java/org/acme/AvengerResourceTest.java
@@ -1,6 +1,8 @@
 package org.acme;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.acme.entity.Avenger;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -11,11 +13,19 @@ public class AvengerResourceTest {
 
     @Test
     public void testAvengerEndpoint() {
+        Avenger avenger = new Avenger("Ant-Man", "Scott Lang", false);
+        given()
+            .contentType(ContentType.JSON)
+            .body(avenger)
+            .when().post("/avenger")
+            .then()
+            .statusCode(201);
+
         given()
           .when().get("/avenger")
           .then()
              .statusCode(200)
-             .body("size()", is(4));
+             .body("size()", is(5));
     }
 
 }
