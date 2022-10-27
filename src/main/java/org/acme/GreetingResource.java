5574b26024f795d68ad5e109ca9ec9443af2a246
xstefank <xstefank122@gmail.com>
2022-10-27 07:38:48 +0200

    Add map support example

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,6 +1,7 @@
 package org.acme;
 
 import org.acme.entity.Avenger;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 
 import javax.transaction.Transactional;
@@ -8,6 +9,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Path("/hello")
@@ -16,11 +18,15 @@ public class GreetingResource {
     private AtomicInteger counter = new AtomicInteger(0);
     private static final Logger LOG = Logger.getLogger(GreetingResource.class);
 
+    @ConfigProperty(name = "my.map")
+    Map<String, String> myMap;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Transactional
     public String hello() {
         LOG.info("Invocation #" + counter.incrementAndGet());
+        LOG.info("my.map = " + myMap.entrySet());
 
         Avenger avenger = new Avenger("Iron Man", "Tony Stark", false);
         avenger.persist();
